5af48c256464985bb4fc52b77ec9254755a2ca7a
Atharva Pagare <atharvapagare7774@gmail.com>
2023-11-15 19:38:17 +0530

    changed logo and link

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
         </p>
         <a
           className="App-link" 
-           href="https://www.propelleraero.com/dirtmate/"
-           target="_blank"
+            href="https://www.propelleraero.com/dirtmate/"
+            target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
